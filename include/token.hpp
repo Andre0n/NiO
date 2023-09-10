f14f597318747e2813a438d2b11797bdb30f0a04
Andre0n <andreon098@gmail.com>
2023-09-10 14:02:29 -0300

    feat(token): use `std::string_view` instead `std::string`

--- include/token.hpp
+++ include/token.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 
 enum class TokenType {
     _error = -2,
@@ -16,7 +17,7 @@ enum class TokenType {
 class Token {
   private:
     TokenType type;
-    std::string value;
+    std::string_view value;
     struct {
         std::size_t line;
         std::size_t column;
@@ -24,9 +25,9 @@ class Token {
 
   public:
     Token();
-    Token(TokenType p_type, const std::string& p_value, std::size_t p_line,
+    Token(TokenType p_type, std::string_view p_value, std::size_t p_line,
           std::size_t p_column)
         : type(p_type), value(std::move(p_value)), loc{p_line, p_column} {};
 
-    auto to_string() -> std::string { return value; };
+    auto to_string() -> std::string_view { return value; };
 };
